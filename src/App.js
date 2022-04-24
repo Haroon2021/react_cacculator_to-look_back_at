c117bfc019ab636362eb0c0b09aef89937fa04b7
Haroon <haroon20@hotmail.co.uk>
2022-04-24 12:22:16 +0100

    got commit to push to github

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles.css"
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  ADD_DIGITS: 'add-digits',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGITS:
+      return state
+
+  }
+
+}
+
+function App() {
+  const[{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+  return (
+    <div className = "calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{operation}</div>
+        
+      </div>
+      <button className = "span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className = "span-two">=</button>
+    </div>
+  )
+}
+
+export default App;
